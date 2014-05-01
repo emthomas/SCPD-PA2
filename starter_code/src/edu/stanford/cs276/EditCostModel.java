df339625e2cdb7e801f65f254d6114ad9e6254ba
emthomas <titbabthomas@hotmail.com>
2014-04-30 22:55:13 -0400

    deleted

--- starter_code/src/edu/stanford/cs276/EditCostModel.java
+++
@@ -1,8 +0,0 @@
-package edu.stanford.cs276;
-
-import java.io.Serializable;
-
-public interface EditCostModel extends Serializable {
-
-	public double editProbability(String original, String R, int distance);
-}
